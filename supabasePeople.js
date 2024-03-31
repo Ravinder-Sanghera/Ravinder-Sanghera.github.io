80b9dc3577b283d0a120ceb898dc3ba107d97c67
Ravinder Sanghera <psyrs19@nottingham.ac.uk>
2024-03-31 15:28:46 +0100

    added search function

--- supabasePeople.js
+++ supabasePeople.js
@@ -1,6 +1,20 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+const supabase = createClient("https://czibkypjyfbtsxpigztg.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6aWJreXBqeWZidHN4cGlnenRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE4MDY1MjYsImV4cCI6MjAyNzM4MjUyNn0.Eizv6pKn1tqGNwJ676SeIqQnKrqNJHy8Uo-Dej6G14s")
 
-function Search(){
-    const searchQuery = document.getElementById('searchInput').value;
-    console.log(searchQuery);
+async function Search(){
+    const searchQuery = document.getElementById("searchInput").value;
+    const { data, error } = await supabase
+    .from("People")
+    .select("Name", "Address", "DOB", "LicenceNumber", "ExpiryDate")
+    .ilike("Name", `%${searchQuery}%`)
+    .eq("LicenceNumber", searchQuery)
+
+    console.log(data);
+    
 }
+
 document.getElementById("searchButton").addEventListener("click", Search);
+
+
+
+
