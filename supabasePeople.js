e619840370ee7b9fb1627496fad18a783a870153
Ravinder Sanghera <psyrs19@nottingham.ac.uk>
2024-03-31 17:18:34 +0100

    AAA

--- supabasePeople.js
+++ supabasePeople.js
@@ -1,15 +1,13 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-const supabase = createClient('https://czibkypjyfbtsxpigztg.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6aWJreXBqeWZidHN4cGlnenRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE4MDY1MjYsImV4cCI6MjAyNzM4MjUyNn0.Eizv6pKn1tqGNwJ676SeIqQnKrqNJHy8Uo-Dej6G14s')
+const supabase = createClient("https://czibkypjyfbtsxpigztg.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6aWJreXBqeWZidHN4cGlnenRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE4MDY1MjYsImV4cCI6MjAyNzM4MjUyNn0.Eizv6pKn1tqGNwJ676SeIqQnKrqNJHy8Uo-Dej6G14s");
 
 async function Search(){
     const searchQuery = document.getElementById("searchInput").value;
 
-    const orQuery = `Name.ilike.%${searchQuery}%,LicenceNumber.eq.${searchQuery}`;
-
     const { data, error } = await supabase
         .from("People")
         .select("*")
-        .or(orQuery);
+        .or(`Name.ilike.%${searchQuery}%,LicenceNumber.eq.${searchQuery}`);
 
         
         
@@ -20,6 +18,4 @@ async function Search(){
     document.getElementById("searchButton").addEventListener("click", Search);
     
     
-    
-// supabase.ilike("Name", "*${searchQuery}*")
 
