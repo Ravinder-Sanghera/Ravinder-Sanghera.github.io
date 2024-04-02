8ff0336f0bb3b4937351c442a02eee54e4e35ab2
Ravinder Sanghera <psyrs19@nottingham.ac.uk>
2024-04-02 09:30:09 +0100

    added new person

--- supabaseNCars.js
+++ supabaseNCars.js
@@ -19,7 +19,67 @@ checkbox.addEventListener('click', function (){
     }
 });
 
+async function newOwner(name){
+    const address = document.getElementById("Address").value;
+    const DOB = document.getElementById("DOB").value;
+    const license = document.getElementById("License").value;
+    const expiry = document.getElementById("Expiry").value;
+
+    const {data, error} = await supabase
+        .from("People")
+        .insert({Name: name, Address: address, DOB: DOB, LicenseNumber: license, ExpiryDate: expiry})
+        .select();
+    if (error){
+        return -1;
+    }else{
+        return data[0].PersonID;
+    }
+}
+
+async function existingOwner(name){
+    const {data, error} = await supabase
+        .from("People")
+        .select("*")
+        .ilike("Name", `%${name}%`);
+    if (error){
+        return -1;
+    }
+    try{
+        return data[0].PersonID;
+    }catch{
+        return -1;
+    }
+}
+
+function notFound(output){
+    const line = document.createElement("p");
+    line.textContent = "Error: Name not Found";
+    output.appendChild(line);
+}
+
+async function Submit(){
+    const output = document.getElementById("error");
+    output.innerHTML = "";
+    const name = document.getElementById("Name").value;
+    const reg = document.getElementById("RegInput").value;
+    const make = document.getElementById("MakeInput").value;
+    const model = document.getElementById("ModelInput").value;
+    const colour = document.getElementById("ColInput").value;
+    let ownerID;
+    if (checkbox.checked){
+        ownerID = await newOwner(name);
+    }else{
+        ownerID = await existingOwner(name);
+        if (ownerID == -1){
+            notFound(output);
+            return;
+        }
+    }
+    const {error} = await supabase
+        .from("Vehicles")
+        .insert({VehicleID: reg, Make: make, Model: model, Colour: colour, OwnerID: ownerID});
+}
 
 
 
-// document.getElementById("submitButton").addEventListener("click", Submit);
+document.getElementById("submitButton").addEventListener("click", Submit);
