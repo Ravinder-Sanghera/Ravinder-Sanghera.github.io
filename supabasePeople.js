1fc64126ec5b31ad8877948bf4ac682cefba0a18
Ravinder Sanghera <psyrs19@nottingham.ac.uk>
2024-05-09 16:15:54 +0100

    person wokrking

--- supabasePeople.js
+++ supabasePeople.js
@@ -2,22 +2,54 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 const supabase = createClient("https://czibkypjyfbtsxpigztg.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6aWJreXBqeWZidHN4cGlnenRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE4MDY1MjYsImV4cCI6MjAyNzM4MjUyNn0.Eizv6pKn1tqGNwJ676SeIqQnKrqNJHy8Uo-Dej6G14s");
 
 async function Search(){
-    const searchQuery = document.getElementById("name").value;
-
-    const {data, error} = await supabase
-        .from("People")
-        .select("*")
-        .or(`Name.ilike.%${searchQuery}%`, `LicenseNumber.eq.${searchQuery}`);
-
-    if (error){
-        console.log("error");
+    const message = document.getElementById("message");
+    message.textContent =  '';
+    
+    const searchQueryP = document.getElementById("name").value;
+    const searchQueryL = document.getElementById("license").value;
+    if (searchQueryL === "" && searchQueryP === ""){
+        message.textContent =  'Error';
         return;
     }
+    else if (searchQueryL != "" && searchQueryP != ""){
+        message.textContent = 'Error';
+        return;
+    }
+    
+    else if (searchQueryL === ""){
+        const {data, error} = await supabase
+            .from("People")
+            .select("*")
+            .ilike(`Name`, `%${searchQueryP}%`);
+    
+        if (error){
+            console.log("error");
+            return;
+        }
 
-    const output = document.getElementById("results");
-    output.innerHTML = "<h2>Results:</h2>";
+        print(data)
+    }
+    else{
+        const {data, error} = await supabase
+            .from("People")
+            .select("*")
+            .eq('LicenseNumber', searchQueryL);
+    
+        if (error){
+            console.log("error");
+            return;
+        }
+        print(data)
+  
+    }
+    
+}
+
+function print(data){
     let found = false;
-
+    const output = document.getElementById("results");
+    const message = document.getElementById("message");
+    output.innerHTML = "<h2>Results:</h2>";
     for (const Row of data){
         const div = document.createElement("div"); // Create a div for each row
         div.textContent = `Name: ${Row.Name},   Address: ${Row.Address},   DOB: ${Row.DOB}, 
@@ -26,17 +58,11 @@ async function Search(){
         found = true;
     }
     if (!found){
-        const line = document.createElement("p");
-        line.textContent = "Error: Not Found";
-        output.appendChild(line);
-        const message = document.getElementById("message");
         message.textContent =  'No result found';
     }
     else{
-        const message = document.getElementById("message");
         message.textContent =  'Search successful';
     }
-    
 }
     
 document.getElementById("searchButton").addEventListener("click", Search);
